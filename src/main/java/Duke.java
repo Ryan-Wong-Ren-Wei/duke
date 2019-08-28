f1c008e10889eb071752b7a5adcef2f5b0ae7251
RyanWong <E0310273@u.nus.edu>
2019-08-28 21:39:01 +0800

    level 3 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
         Scanner reader = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,12 +20,22 @@ public class Duke {
                 System.out.print(lineSeparation);
                 for (int i = 0; i < tasks.size(); ++i) {
                     int j = i + 1;
-                    System.out.println(j + ". " + tasks.get(i));
+                    System.out.println(j + ".[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).getDescription());
                 }
                 System.out.print(lineSeparation);
                 userInput = reader.nextLine();
+
+            } else if (userInput.length() > 4 && userInput.substring(0,4).equals("done")) {
+                int taskNo = Integer.parseInt(userInput.substring(5));
+                tasks.get(taskNo - 1).markAsDone();
+                System.out.print(lineSeparation);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[\u2713] " + tasks.get(taskNo - 1).getDescription());
+                System.out.print(lineSeparation);
+                userInput = reader.nextLine();
+
             } else {
-                tasks.add(userInput);
+                tasks.add(new Task(userInput));
                 System.out.print(lineSeparation + "added: " + userInput + "\n" + lineSeparation);
                 userInput = reader.nextLine();
             }
