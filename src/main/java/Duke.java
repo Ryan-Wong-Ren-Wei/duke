6e5d5b7e528ff7920d327814183c72094cf6d880
RyanWong <E0310273@u.nus.edu>
2019-08-28 21:22:01 +0800

    level 2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> tasks;
+        ArrayList<String> tasks = new ArrayList<String>();
         Scanner reader = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,9 +15,20 @@ public class Duke {
         System.out.println(lineSeparation + "Hello! I'm Duke\nWhat can i do for you?\n" + lineSeparation);
         String userInput = reader.nextLine();
 
-        while (!userInput.equals("bye")){
-            System.out.print(lineSeparation + userInput + "\n" + lineSeparation);
-            userInput = reader.nextLine();
+        while (!userInput.equals("bye")) {
+            if (userInput.equals("list")) {
+                System.out.print(lineSeparation);
+                for (int i = 0; i < tasks.size(); ++i) {
+                    int j = i + 1;
+                    System.out.println(j + ". " + tasks.get(i));
+                }
+                System.out.print(lineSeparation);
+                userInput = reader.nextLine();
+            } else {
+                tasks.add(userInput);
+                System.out.print(lineSeparation + "added: " + userInput + "\n" + lineSeparation);
+                userInput = reader.nextLine();
+            }
         }
 
         System.out.println(lineSeparation + "Bye. Hope to see you again soon!" + "\n" + lineSeparation);
