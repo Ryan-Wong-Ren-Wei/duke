ec37fa8b51c0dc3b5beea1048f4fcf27f32f1e88
RyanWong <E0310273@u.nus.edu>
2019-08-28 21:10:43 +0800

    level 1 really completed this time

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> tasks;
         Scanner reader = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,9 +11,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         String lineSeparation = "____________________________________________________________\n";
-
         System.out.println(lineSeparation + "Hello! I'm Duke\nWhat can i do for you?\n" + lineSeparation);
+        String userInput = reader.nextLine();
+
+        while (!userInput.equals("bye")){
+            System.out.print(lineSeparation + userInput + "\n" + lineSeparation);
+            userInput = reader.nextLine();
+        }
+
+        System.out.println(lineSeparation + "Bye. Hope to see you again soon!" + "\n" + lineSeparation);
     }
 }
