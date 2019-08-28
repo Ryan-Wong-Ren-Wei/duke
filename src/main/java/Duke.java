9dc971ac972b6c103cec33cc840998831e83c4f5
RyanWong <E0310273@u.nus.edu>
2019-08-29 00:50:00 +0800

    level 4 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,12 +20,12 @@ public class Duke {
                 System.out.print(lineSeparation);
                 for (int i = 0; i < tasks.size(); ++i) {
                     int j = i + 1;
-                    System.out.println(j + ".[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).getDescription());
+                    System.out.println(j + "." + tasks.get(i).toString());
                 }
                 System.out.print(lineSeparation);
                 userInput = reader.nextLine();
 
-            } else if (userInput.length() > 4 && userInput.substring(0,4).equals("done")) {
+            } else if (userInput.length() >= 4 && userInput.substring(0,4).equals("done")) {
                 int taskNo = Integer.parseInt(userInput.substring(5));
                 tasks.get(taskNo - 1).markAsDone();
                 System.out.print(lineSeparation);
@@ -34,9 +34,33 @@ public class Duke {
                 System.out.print(lineSeparation);
                 userInput = reader.nextLine();
 
-            } else {
-                tasks.add(new Task(userInput));
-                System.out.print(lineSeparation + "added: " + userInput + "\n" + lineSeparation);
+            } else if (userInput.length() >= 4 && userInput.substring(0,4).equals("todo")){
+                tasks.add(new ToDo(userInput.substring(5)));
+                System.out.print(lineSeparation);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("[T][\u2718] " + userInput.substring(5));
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                System.out.print(lineSeparation);
+                userInput = reader.nextLine();
+
+            } else if (userInput.length() >= 8 && userInput.substring(0,8).equals("deadline")){
+                int slashPos = userInput.indexOf("/by");
+                tasks.add(new Deadline(userInput.substring(9,slashPos), userInput.substring(slashPos+4)));
+                System.out.print(lineSeparation);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks.get(tasks.size()-1).toString());
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                System.out.print(lineSeparation);
+                userInput = reader.nextLine();
+
+            } else if (userInput.length() >= 5 && userInput.substring(0,5).equals("event")){
+                int slashPos = userInput.indexOf("/at");
+                tasks.add(new Event(userInput.substring(9,slashPos), userInput.substring(slashPos+4)));
+                System.out.print(lineSeparation);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks.get(tasks.size()-1).toString());
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                System.out.print(lineSeparation);
                 userInput = reader.nextLine();
             }
         }
