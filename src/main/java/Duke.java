727cccb31e60952820a0da1f3764683a96f1720f
RyanWong <E0310273@u.nus.edu>
2019-08-29 01:04:00 +0800

    level 5 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,48 +23,72 @@ public class Duke {
                     System.out.println(j + "." + tasks.get(i).toString());
                 }
                 System.out.print(lineSeparation);
-                userInput = reader.nextLine();
 
-            } else if (userInput.length() >= 4 && userInput.substring(0,4).equals("done")) {
-                int taskNo = Integer.parseInt(userInput.substring(5));
-                tasks.get(taskNo - 1).markAsDone();
-                System.out.print(lineSeparation);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[\u2713] " + tasks.get(taskNo - 1).getDescription());
-                System.out.print(lineSeparation);
-                userInput = reader.nextLine();
+            } else if (userInput.length() >= 4 && userInput.substring(0, 4).equals("done")) {
+                if (userInput.equals("done")) {
+                    System.out.print(lineSeparation);
+                    System.out.println("☹ OOPS!!! The description of a completed task cannot be empty.");
+                    System.out.print(lineSeparation);
+                } else {
+                    int taskNo = Integer.parseInt(userInput.substring(5));
+                    tasks.get(taskNo - 1).markAsDone();
+                    System.out.print(lineSeparation);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[\u2713] " + tasks.get(taskNo - 1).getDescription());
+                    System.out.print(lineSeparation);
+                }
 
-            } else if (userInput.length() >= 4 && userInput.substring(0,4).equals("todo")){
-                tasks.add(new ToDo(userInput.substring(5)));
-                System.out.print(lineSeparation);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("[T][\u2718] " + userInput.substring(5));
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                System.out.print(lineSeparation);
-                userInput = reader.nextLine();
+            } else if (userInput.length() >= 4 && userInput.substring(0, 4).equals("todo")) {
+                if (userInput.equals("todo")) {
+                    System.out.print(lineSeparation);
+                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                    System.out.print(lineSeparation);
+                } else {
+                    tasks.add(new ToDo(userInput.substring(5)));
+                    System.out.print(lineSeparation);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("[T][\u2718] " + userInput.substring(5));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.print(lineSeparation);
+                }
 
-            } else if (userInput.length() >= 8 && userInput.substring(0,8).equals("deadline")){
-                int slashPos = userInput.indexOf("/by");
-                tasks.add(new Deadline(userInput.substring(9,slashPos), userInput.substring(slashPos+4)));
-                System.out.print(lineSeparation);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks.get(tasks.size()-1).toString());
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                System.out.print(lineSeparation);
-                userInput = reader.nextLine();
+            } else if (userInput.length() >= 8 && userInput.substring(0, 8).equals("deadline")) {
+                if (userInput.equals("deadline")) {
+                    System.out.print(lineSeparation);
+                    System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+                    System.out.print(lineSeparation);
+                } else {
+                    int slashPos = userInput.indexOf("/by");
+                    tasks.add(new Deadline(userInput.substring(9, slashPos), userInput.substring(slashPos + 4)));
+                    System.out.print(lineSeparation);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks.get(tasks.size() - 1).toString());
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.print(lineSeparation);
+                }
 
-            } else if (userInput.length() >= 5 && userInput.substring(0,5).equals("event")){
-                int slashPos = userInput.indexOf("/at");
-                tasks.add(new Event(userInput.substring(9,slashPos), userInput.substring(slashPos+4)));
+            } else if (userInput.length() >= 5 && userInput.substring(0, 5).equals("event")) {
+                if (userInput.equals("event")) {
+                    System.out.print(lineSeparation);
+                    System.out.println("☹ OOPS!!! The description of an event cannot be empty.");
+                    System.out.print(lineSeparation);
+                } else {
+                    int slashPos = userInput.indexOf("/at");
+                    tasks.add(new Event(userInput.substring(9, slashPos), userInput.substring(slashPos + 4)));
+                    System.out.print(lineSeparation);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks.get(tasks.size() - 1).toString());
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.print(lineSeparation);
+                }
+            } else {
                 System.out.print(lineSeparation);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks.get(tasks.size()-1).toString());
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 System.out.print(lineSeparation);
-                userInput = reader.nextLine();
             }
-        }
 
+            userInput = reader.nextLine();
+        }
         System.out.println(lineSeparation + "Bye. Hope to see you again soon!" + "\n" + lineSeparation);
     }
 }
